075a8e5b350cfa750ceb65169fb54db712e5c842
Tom Lebon <tlebon@paul-f4Ar7s7.clusters.42paris.fr>
2024-02-16 15:34:48 +0100

    Creation du dossier lib

---
+++ lib/gnl/get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:55:48 by tlebon            #+#    #+#             */
+/*   Updated: 2024/01/24 19:50:54 by tlebon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*lucas_safe(char *s1, char *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+
+char	*get_next_line(int fd);
+
+#endif 
