d72ee98176f7183e1fd0f03d5303e96cb1602d9f
Tom Lebon <tlebon@paul-f4Ar5s9.clusters.42paris.fr>
2024-02-19 23:53:22 +0100

    Pas de changements je crois

--- Projets/fract-ol/src/main.c
+++ Projets/fract-ol/src/main.c
@@ -6,11 +6,16 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:55:43 by tlebon            #+#    #+#             */
-/*   Updated: 2024/02/17 22:53:54 by tlebon           ###   ########.fr       */
+/*   Updated: 2024/02/19 23:52:46 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
+
+// void	free_all(t_mlx_data *s_mlx)
+// {
+// 	ft_putstr("C'est pas free boufon !\n");
+// }
 
 // Renvoie un int valant la couleur RGB
 int	create_trgb(int t, int r, int g, int b)
@@ -87,34 +92,14 @@ int	draw_fract_on_img(t_img_data *s_img, int x_size, int y_size, int max_iterati
 
 	return (0);
 }
-// int	draw_fract_on_img2(t_img_data *s_img, int max_iterations)
+// int	key_hook(int keycode, t_mlx_data *s_mlx)
 // {
-// 	// xn definit la plage du plan complex a afficher
-// 	float	x1 = -2.1;
-// 	float	x2 = 0.6;
-// 	float	y1 = -1.2;
-// 	float	y2 = 1.2;
-// 	int		zoom = 300;
-// 	// I guess defini le facteur d'agrandissement
-// 	float	x_size = (x2 - x1) * zoom;
-// 	float	y_size = (y2 - y1) * zoom;
-// 	// Les coordonnes dans l'image
-// 	int	x = 0;
-//		
-// 	while (x < x_size)
+// 	if (keycode == 27)
 // 	{
-// 		int	y = 0;
-// 		while (y < y_size)
-// 		{
-// 			if (calc_for_point((x / zoom) + x1, (y / zoom) + y1, max_iterations) == max_iterations)
-// 				put_pixel_to_image(s_img, x, y, 0x00000000);
-// 			else
-// 				put_pixel_to_image(s_img, x, y, 0xFF4287F5);
-// 			y++;
-// 		}
-// 		x++;
-// 	}	
-// 	return (0);
+// 		mlx_destroy_window(s_mlx->mlx, s_mlx->win);
+// 		free_all(s_mlx);
+// 		return (0);	
+// 	}
 // }
 
 // int	main()
@@ -123,13 +108,18 @@ int	main(int ac, char *av[])
 	const int	X_RES = 1920;
 	const int	Y_RES = 1080;
 	// Gestion des mauvaises entrees
-	if (ac < 2)
+	if (ac > 1 && av[1][0] == 'm')
+		ft_putstr("Ensemble de Mandelbrot selectionne\n");
+	else if (ac > 1 && av[1][0] == 'j')
 	{
+		if (ac < 3 || !ft_isdigit(ft_atoi(av[2])))
+		{
 		ft_putstr("\033[1;31mNot enough arguments\n\033[0mThe \033[1;32mcorrect\033[0m use is:\n./fractol m\n./fractol j <number>\n");
 		return (1);
+		}
+		ft_putstr("Ensemble de Julia selectionne\n");
 	}
-	else if (av[1][0] == 'm')
-		ft_putstr("Ensemble de Mandelbrot selectionne\n");
+
 	////////////////////////////////
 	t_mlx_data	s_mlx;
 	t_img_data	s_img;
@@ -141,7 +131,7 @@ int	main(int ac, char *av[])
 	s_mlx.win = mlx_new_window(s_mlx.mlx, X_RES, Y_RES, "Fractol");
 	if (!s_mlx.win)
 		return (1);
-	
+	// mlx_hook(s_mlx.win, 2, 1L << 0, keyhook(), &s_mlx);
 	// Creation of an image
 	s_img.img = mlx_new_image(s_mlx.mlx, X_RES, Y_RES);
 	if(!s_img.img)
