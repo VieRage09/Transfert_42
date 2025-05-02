a3d0f08452b3bf1e933a275cea36b8f82cec20d6
VieRage09 <tom.lebon@hotmail.com>
2025-05-02 03:22:01 +0200

    CPP/CPP07 termine, Reste a tout bien check notemment fonction size de l'exo 2

--- Projets/CPP/CPP07/ex02/Array.hpp
+++ Projets/CPP/CPP07/ex02/Array.hpp
@@ -6,41 +6,94 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 19:21:09 by tlebon            #+#    #+#             */
-/*   Updated: 2025/04/30 20:28:50 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/02 03:18:54 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	ARRAY_HPP
-#define	ARRAY_HPP
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
 
 // includes //
 #include <iostream>
 
-template<typename T>
+template <typename T>
 class Array
 {
-	private:
+private:
+	// attributes //
+	T *_parray;
+	unsigned int _length;
 
-		// attributes //
-		T				*_parray;
-		unsigned int	_length;
+public:
+	// Default constructor
+	Array() : _length(0)
+	{
+		_parray = new T[0];
+		std::cout << "Empty array constructed\n";
+	}
 
-	public:
-		// Default constructor
-		Array();
-		// Personnalized constructor
-		Array(unsigned int n);
-		// Copy constructor
-		Array(const Array& copy);
-		// Destructor
-		~Array();
+	// Personnalized constructor
+	Array(unsigned int n) : _length(n)
+	{
+		_parray = new T[n];
+		std::cout << "Array of size " << _length << " constructed\n";
+	}
 
-		// Methods //
-		unsigned int	size( void ) const;
+	// Copy constructor
+	Array(const Array & copy) : Array(copy._length)
+	{
+		for (int i = 0; i < copy._length; i++)
+			_parray[i] = copy[i];
+		std::cout << "Array of length " << copy._length << " copied\n";
+	}
 
-		// Operators //
-		Array&			operator = (const Array& copy);
-		T&				operator [] (unsigned int index);
+	// Destructor
+	~Array()
+	{
+		delete[] _parray;
+		std::cout << "Array of length " << _length << " destroyed\n";
+	}
+
+	// Methods //
+	unsigned int size(void) const
+	{
+		int size = 0;
+		for (int i = 0; i < _length; i++)
+			if (_parray[i])
+				size++;
+		return (size);
+	}
+
+	void display_array(void) const
+	{
+		std::cout << "Displaying array's values: \n";
+		for (int i = 0; i < _length; i++)
+			std::cout << i << "; " << _parray[i] << std::endl;
+	}
+
+	// Getters //
+	const unsigned int get_length(void) const { return (_length); }
+
+	// Operators //
+	Array &	operator=(const Array<T> & copy)
+	{
+		if (this != &copy)
+		{
+			delete[] _parray;
+			_parray = new T[copy._length];
+			_length = copy._length;
+			for (int i = 0; i < copy._length; i++)
+				_parray[i] = copy[i];
+		}
+		return (*this);
+	}
+
+	T &		operator[](unsigned int index) const
+	{
+		if (index >= _length && _length > 0)
+			throw std::out_of_range("Index is out of range");
+		return (_parray[index]);
+	}
 
 };
 
--- Projets/CPP/CPP07/ex02/main.cpp
+++ Projets/CPP/CPP07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 20:23:06 by tlebon            #+#    #+#             */
-/*   Updated: 2025/04/30 20:28:46 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/02 03:19:28 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,52 @@
 
 int	main( void )
 {
-	int * a = new int();
-	std::cout << *a << std::endl;
+	std::cout << "Creation of a empty int array\n";
+	Array<int> aint = Array<int>();
+	std::cout << "Displaying array[0]: " << aint[0] << std::endl;
+	
+	unsigned int	size = 5;
+	std::cout << "\nCreation of a float array of size " << size << std::endl;
+	Array<float> afloat5 = Array<float>(size);
+	afloat5.display_array();
+	std::cout << "\nModifying values of float array\n";
+	afloat5[2] = 4;
+	afloat5[3] = 4.14;
+	afloat5.display_array();
+
+	std::cout << "\nTesting copy const and assignement operator\n";
+	Array<float> cpy = Array<float>(afloat5);
+	Array<float> assign = (afloat5);
+	std::cout << "Copy array:\n";
+	cpy.display_array();
+	std::cout << "Assign array:\n";
+	assign.display_array();
+	std::cout << "Changing values\n";
+	cpy[0] = 12.1;
+	assign[3] = 9.04;
+	std::cout << "OG array:\n";
+	afloat5.display_array();
+	std::cout << "Copy array:\n";
+	cpy.display_array();
+	std::cout << "Assign array:\n";
+	assign.display_array();
+
+	std::cout << "\nTesting exception on operator []\n";
+	try
+	{
+		afloat5[8] = 2;
+		std::cout << "Problems\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\nSize function\n";
+	std::cout << "empty int array size: " << aint.size() << std::endl;
+	std::cout << "float array size: " << afloat5.size() << std::endl;
+	std::cout << "copy array size: " << cpy.size() << std::endl;
+	
 
 	return (0);
 }
