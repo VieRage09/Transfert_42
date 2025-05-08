bb3b09daadf50616f757269b4ee67a49829d04f6
VieRage09 <tom.lebon@hotmail.com>
2025-05-08 18:55:43 +0200

    CPP/CPP08/ex02: Fonctionnement de std::stack replique, reste a ajouter les iterators

--- Projets/CPP/CPP08/ex02/MutantStack.hpp
+++ Projets/CPP/CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 16:01:52 by tlebon            #+#    #+#             */
-/*   Updated: 2025/05/06 16:17:31 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/08 18:54:49 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,43 @@
 
 // includes //
 #include <iostream>
-#include <stack>
 #include <deque>
 
 template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack
+class MutantStack
 {
 	private:
-
 		// attributes //
+		Container stk;
 
 	public:
 		// Default constructor
-		MutantStack() : Container() {std::cout << "Mutant stack created\n";}
+		MutantStack() : stk(Container()) {std::cout << "Empty MutantStack created\n";}
+		// Parametric constructor
+		MutantStack(size_t size, T init_value) : stk(Container(size, init_value))
+		{
+			std::cout	<< "MutantStack of size " << size
+						<< " created with initialization value of " << init_value
+						<< std::endl;
+		}
 		// Copy constructor
-		MutantStack(const MutantStack& copy) : Container(copy) {std::cout << "Mutant stack copied\n";}
+		MutantStack(const MutantStack& copy) : stk(Container(copy)) {std::cout << "Mutant stack copied\n";}
 		// Destructor
-		~MutantStack() : ~Container() {std::cout << "Mutant stack deleted\n";}
+		~MutantStack() {std::cout << "Mutant stack deleted\n";}
 
 		// Methods //
-		Container::iterator &	begin() const
-		{
-			return (this->begin());
-		}	
 
-		Container::iterator &	end()
-		{
-			return (this->end());
-		}	
-
-		void MutantStack::printoui( void ) {std::cout << "oui\n";}
-		// Container::iterator &	begin()
-		// {
-
-		// }	
-		// Container::iterator &	begin()
-		// {
-
-		// }	
+		bool		empty( void ) const {return (stk.empty());}
+		auto		size( void ) const {return (stk.size());}
+		T &			top( void ) {return (stk.back());}
+		void		push(const T & value) {stk.push_back(value);}
+		template <typename... Args>
+		void		emplace(Args&&... args) {stk.emplace_back(args...);}
+		void		pop( void ) {stk.pop_back();}
+		void		swap(MutantStack<T, Container> & other) noexcept {stk.swap(other.get_stk());}
 
 		// Operators //
+
 		// MutantStack&		operator = (const MutantStack& copy)
 		// {
 		// 	if (*this != copy)
@@ -63,6 +60,9 @@ class MutantStack : public std::stack
 		// 	}
 		// 	return (*this);
 		// }
+
+		// Getters //
+		auto &	get_stk( void ) {return (stk);}
 
 };
 
--- Projets/CPP/CPP08/ex02/main.cpp
+++ Projets/CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 16:01:49 by tlebon            #+#    #+#             */
-/*   Updated: 2025/05/06 16:14:02 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/08 18:53:46 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,44 @@ int	main ( void )
 {
 	MutantStack<int> ms;
 	
-	ms.beg();
-	ms.;
+	if (ms.empty())
+		std::cout << "MutantStack is empty\nSize = " << ms.size() << std::endl;
+	std::cout << "Pushing 1 to ms\n";
+	ms.push(1);
+	std::cout	<< std::boolalpha << "Empty: " << ms.empty() << std::endl
+				<< "Size: " << ms.size() << std::endl
+				<< "Top: " << ms.top() << std::endl;
+	std::cout << "Calling pop on ms\n";
+	ms.pop();
+	std::cout	<< std::boolalpha << "Empty: " << ms.empty() << std::endl
+				<< "Size: " << ms.size() << std::endl;
+
+	MutantStack<std::string> mss1(3, "Hello there !");
+	std::cout	<< std::boolalpha << "Empty: " << mss1.empty() << std::endl
+				<< "Size: " << mss1.size() << std::endl
+				<< "Top: " << mss1.top() << std::endl;
+	std::cout << "Poping 2 elements then emplace General Kenobi\n";
+	mss1.pop();
+	mss1.pop();
+	mss1.emplace("General Kenobi");
+	std::cout	<< std::boolalpha << "Empty: " << mss1.empty() << std::endl
+				<< "Size: " << mss1.size() << std::endl
+				<< "Top: " << mss1.top() << std::endl;
+	MutantStack<std::string> mss2(1, "AAAh the negociator");
+	std::cout	<< std::boolalpha << "Empty: " << mss2.empty() << std::endl
+				<< "Size: " << mss2.size() << std::endl
+				<< "Top: " << mss2.top() << std::endl;
+	mss2.swap(mss1);
+	std::cout 	<< "mss1 infos:\n";
+	std::cout	<< std::boolalpha << "Empty: " << mss1.empty() << std::endl
+				<< "Size: " << mss1.size() << std::endl
+				<< "Top: " << mss1.top() << std::endl;
+	std::cout 	<< "mss2 infos:\n";
+	std::cout	<< std::boolalpha << "Empty: " << mss2.empty() << std::endl
+				<< "Size: " << mss2.size() << std::endl
+				<< "Top: " << mss2.top() << std::endl;
+
 	return (0);
 }
+
+// Doc: calling top on empty stack is undefined behavior
