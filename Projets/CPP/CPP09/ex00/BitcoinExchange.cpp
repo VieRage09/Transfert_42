a7433a4514fcf40b3ce065fcf9545010fd06e83a
Tom Lebon <tlebon@f2r12s15.clusters.42paris.fr>
2025-05-19 18:57:38 +0200

    CPP/CPP09/ex00: Quasi termine juste voir pour les tests de parsing de nazi

--- Projets/CPP/CPP09/ex00/BitcoinExchange.cpp
+++ Projets/CPP/CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 18:18:37 by tlebon            #+#    #+#             */
-/*   Updated: 2025/05/16 23:03:43 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/19 18:55:22 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,21 @@ BitcoinExchange::BitcoinExchange( void ) : _db_map() {std::cout << "Default Bitc
 BitcoinExchange::BitcoinExchange(std::string db_path) : _db_map()
 {
 	std::string		line;
-	std::fstream	db(db_path);
+	std::ifstream	db(db_path);
 
-	// ATTENTION AVANT --> Check si le fichier a bien ete ouvert
+	if (db.fail())
+		throw std::runtime_error("File does not exist");
 	while (!db.eof())
 	{
 		std::getline(db, line);
 		if (line.find_first_of("0123456789") == std::string::npos)
 		{
-			std::cout << "Line skipped\n";
+			// std::cout << "Line skipped\n";
 			continue;
 		}
 		_db_map.insert(create_pair(line, ','));
 	}
+	db.close();
 	std::cout << "BitcoinExchange object instanciated with " << db_path << " as input\nSize = " << _db_map.size() << std::endl;
 }
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) : _db_map(copy._db_map) {std::cout << "BitcoinExchange instance copied\n";}
@@ -66,37 +68,33 @@ void	BitcoinExchange::display_closest_value(std::pair<time_t, float> pair)
 	struct tm 	*date;
 	char		str[11];
 	float		value;
-
+	
+	std::map<time_t, float>::iterator up_bound = _db_map.upper_bound(pair.first);
+	up_bound--;
 	date = gmtime(&(pair.first));
 	if (strftime(str, 11, "%Y-%m-%d", date) == 0)
 	{
 		throw std::runtime_error("[ERROR] strftime");
 		return ;
 	}
-	std::cout << str << " => " << pair.second << " = ";
-	try
-	{
-		value = _db_map.at(pair.first);
-		std::cout << value * pair.second << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		// Comparer pour chopper la date inferieure la plus proche
-		
-		
-		// std::cout << value * _db_map.lower_bound(value)->second << std::endl;
-	}
+	std::cout	<< str << " => " << pair.second << " = "
+				<< up_bound->second * pair.second
+				<< std::endl;
 }
 
 void	BitcoinExchange::cmpdisplay_file_values(std::string path)
 {
 	if (path.empty())
 		throw std::runtime_error("[ERROR] Wrong file path\n");
-	std::fstream				input(path);
+	std::ifstream				input(path);
 	std::string					line;
 	std::pair<time_t, float> 	tmp;
 
-	// Check si le fichier existe et a bien ete ouvert
+	if (input.fail())
+	{
+		throw std::runtime_error("[ERROR] File does not exist");
+		return ;
+	}
 	while (!input.eof())
 	{
 		std::getline(input, line);
@@ -112,8 +110,7 @@ void	BitcoinExchange::cmpdisplay_file_values(std::string path)
 			throw std::runtime_error("[ERROR] Too large number\n");
 		display_closest_value(tmp);
 	}
-
-
+	input.close();
 }
 
 // OPERATORS //
--- Projets/CPP/CPP09/ex00/main.cpp
+++ Projets/CPP/CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:07:26 by tlebon            #+#    #+#             */
-/*   Updated: 2025/05/16 22:39:45 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/05/19 17:57:02 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	main (int ac, char **av)
 {
 	if (ac != 2)
 		return (std::cerr << "Usage: ./btc <file>\n", 1);
-	BitcoinExchange	btx("data.csv");
 
 	try
 	{
+		BitcoinExchange	btx("data.csv");
 		btx.cmpdisplay_file_values(av[1]);
 	}
 	catch(const std::exception& e)
