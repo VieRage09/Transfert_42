0b9ec47473ccb03ac6898d2dab01c1fa80c1bba4
Tom Lebon <tlebon@f4r5s3.clusters.42paris.fr>
2025-06-25 18:24:13 +0200

    CPP/CPP09/ex02: Petit menage de printemps + avance sur deque. Avant modif de insert_vec pour eviter les Vec_pair de a et b

--- Projets/CPP/CPP09/ex02/PmergeMe.cpp
+++ Projets/CPP/CPP09/ex02/PmergeMe.cpp
@@ -6,14 +6,14 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 16:13:02 by tlebon            #+#    #+#             */
-/*   Updated: 2025/06/10 13:19:39by tlebon           ###   ########.fr       */
+/*   Updated: 2025/06/25 18:20:33 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-// ############################## CONSTRUCTORS & DESTRUCTORS #####################################//
-
+// ############################## CONSTRUCTORS & DESTRUCTORS ################ //
+#pragma region construtors
 PmergeMe::PmergeMe(char **list)
 	: _vec(std::vector<int>()), _deq(std::deque<int>())
 {
@@ -42,23 +42,12 @@ PmergeMe::PmergeMe(const PmergeMe &copy)
 }
 
 PmergeMe::~PmergeMe() { std::cout << "PmergeMe object destroyed\n"; }
+#pragma endregion constructors
+// ############################## METHODS ################################### //
 
-// ############################### METHODS #######################################################//
+// ------------------------------ PRIVATE METHODS --------------------------- //
+#pragma region private_methods
 
-// PRIVATE METHODS //
-
-
-// Returns the maximum number of comparisons for a given size
-// This is based on the formula derived from the Ford-Johnson algorithm
-unsigned int	PmergeMe::get_max_nb_comps(unsigned int size) const
-{
-	int sum = 0;
-    for (int k = 1; k <= size; ++k) {
-        double value = (3.0 / 4.0) * k;
-        sum += static_cast<int>(ceil(log2(value)));
-    }
-    return sum;
-}
 
 // Used to check if we can increment pos n times w/o getting past end()
 template <typename T>
@@ -94,113 +83,108 @@ void PmergeMe::sort_pairs(unsigned int pair_size)
 	}
 }
 
-// Returns the nth Jacobsthal number
-long	PmergeMe::get_nth_jacobsthal(unsigned int n) const
-{
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return ((pow(2, n) - pow(-1, n)) / 3);
-}
-
 // Uses binary search to find the position to insert the element in the main vector
-Vec_pair::iterator PmergeMe::binary_search(Vec_pair &main, const std::pair<int, std::vector<int>> &elem,
-                                                Vec_pair::iterator lower_bound, Vec_pair::iterator upper_bound)
+Vec_pair::iterator PmergeMe::binary_search(Vec_pair &main,
+									const std::pair<int, std::vector<int>> &elem,
+									Vec_pair::iterator lower_bound,
+									Vec_pair::iterator upper_bound)
 {
-    int value = elem.second.back();
-    auto first = lower_bound;
-    auto last = upper_bound;
+	int value = elem.second.back();
+	auto first = lower_bound;
+	auto last = upper_bound;
 
-    while (first < last) {
-        auto mid = first + (last - first) / 2;
-        int mid_value = mid->second.back();
+	while (first < last)
+	{
+		auto mid = first + (last - first) / 2;
+		int mid_value = mid->second.back();
 		incr_nb_comps();
-        if (value > mid_value)
-            first = mid + 1;
-        else
-            last = mid;
-    }
-    return first;
+		if (value > mid_value)
+			first = mid + 1;
+		else
+			last = mid;
+	}
+	return first;
 }
 
-
-void	PmergeMe::binary_insert(std::pair<int, std::vector<int>> &elem, Vec_pair &main)
+void PmergeMe::binary_insert(std::pair<int, std::vector<int>> &elem, Vec_pair &main)
 {
-	Vec_pair::iterator	insert_pos;
-   	Vec_pair::iterator	upper_bound = main.begin();
+	Vec_pair::iterator insert_pos;
+	Vec_pair::iterator upper_bound = main.begin();
 
 	if (elem.second.empty())
 	{
-        std::cerr << "Error: trying to insert an empty element (index = " << elem.first << ") in binary_insert" << std::endl;
-        return;
-    }
-    for(; upper_bound != main.end(); upper_bound++)
-        if ((*upper_bound).first == elem.first)
-            break;
-    if (upper_bound == main.end())
-    {
-        if (main.empty()) {
-            std::cerr << "Error: main is empty in binary_insert" << std::endl;
-            return;
-        }
-        if (main.size() == 1)
-          	upper_bound = main.begin();
-        else {
-            if (upper_bound == main.begin())
-                upper_bound = main.begin();
-            else
-                upper_bound--;
-        }
-        std::cout << "Upper bound not found, using last element" << std::endl;
-    }
-    std::cout << "Upper bound found: index = " << (*upper_bound).first << std::endl;
-    insert_pos = binary_search(main, elem, main.begin(), upper_bound);
-    main.insert(insert_pos, elem);
+		std::cerr	<< "Error: trying to insert an empty element (index = "
+					<< elem.first << ") in binary_insert" << std::endl;
+		return;
+	}
+	for (; upper_bound != main.end(); upper_bound++)
+		if ((*upper_bound).first == elem.first)
+			break;
+	if (upper_bound == main.end())
+	{
+		if (main.empty())
+		{
+			std::cerr << "Error: main is empty in binary_insert" << std::endl;
+			return;
+		}
+		if (main.size() == 1)
+			upper_bound = main.begin();
+		else
+		{
+			if (upper_bound == main.begin())
+				upper_bound = main.begin();
+			else
+				upper_bound--;
+		}
+		std::cout << "Upper bound not found, using last element" << std::endl;
+	}
+	std::cout << "Upper bound found: index = " << (*upper_bound).first << std::endl;
+	insert_pos = binary_search(main, elem, main.begin(), upper_bound);
+	main.insert(insert_pos, elem);
 }
 
 void PmergeMe::insert_pend(Vec_pair &main, Vec_pair &pend, Vec_pair &a_s)
 {
-    int n = 3;
-    unsigned int jacobsthal = get_nth_jacobsthal(n);
+	int n = 3;
+	unsigned int jacobsthal = get_nth_jacobsthal(n);
 
-    while (!pend.empty())
-    {
-        unsigned int prev_jacob = get_nth_jacobsthal(n - 1);
-        unsigned int count = jacobsthal - prev_jacob;
+	while (!pend.empty())
+	{
+		unsigned int prev_jacob = get_nth_jacobsthal(n - 1);
+		unsigned int count = jacobsthal - prev_jacob;
 
-        if (count > pend.size())
-            count = pend.size();
-        for (int i = count - 1; i >= 0; --i)
-        {
-            std::cout << "Inserting pend element: index = " << pend[i].first << std::endl;
-            std::cout << "Elements: ";
-            for (std::vector<int>::iterator it = pend[i].second.begin(); it != pend[i].second.end(); ++it)
-                std::cout << *it << " ";
-            std::cout << std::endl;
-            binary_insert(pend[i], main);
-        }
-        pend.erase(pend.begin(), pend.begin() + count);
-        jacobsthal = get_nth_jacobsthal(++n);
-    }
+		if (count > pend.size())
+			count = pend.size();
+		for (int i = count - 1; i >= 0; --i)
+		{
+			std::cout << "Inserting pend element: index = " << pend[i].first << std::endl;
+			std::cout << "Elements: ";
+			for (std::vector<int>::iterator it = pend[i].second.begin(); it != pend[i].second.end(); ++it)
+				std::cout << *it << " ";
+			std::cout << std::endl;
+			binary_insert(pend[i], main);
+		}
+		pend.erase(pend.begin(), pend.begin() + count);
+		jacobsthal = get_nth_jacobsthal(++n);
+	}
 }
 
 // Creates and loads 2 vectors (main & pend) then insert the pend into main and update _vec in the end
-void	PmergeMe::insert_vec(unsigned int elem_size)
+void PmergeMe::insert_vec(unsigned int elem_size)
 {
-	Vec_pair			a_s;
-	Vec_pair			b_s;
-	Vec_pair			main;
-	Vec_pair			pend;
-	std::vector<int>	remains;
+	Vec_pair a_s;
+	Vec_pair b_s;
+	Vec_pair main;
+	Vec_pair pend;
+	std::vector<int> remains;
 
-	bool	is_biggest_in_pair = false;
-	int		index = 1;
+	bool is_biggest_in_pair = false;
+	int index = 1;
 	// Loading a_s and b_s so we can keep for later their labels
 	std::vector<int>::iterator it = _vec.begin();
 	for (; safe_advance<std::vector<int>>(it, elem_size); it += elem_size)
 	{
-		std::vector<int>	group(it, it + elem_size);
+		std::vector<int> group(it, it + elem_size);
 		if (group.empty())
 			continue;
 		if (!is_biggest_in_pair)
@@ -216,15 +200,15 @@ void	PmergeMe::insert_vec(unsigned int elem_size)
 		for (auto it_elem = it; it_elem != _vec.end(); it_elem++)
 			remains.push_back(*it_elem);
 
-	// Loading main and pend vectors 
+	// Loading main and pend vectors
 	if (!b_s.empty() && !b_s.front().second.empty())
 		main.push_back(b_s.front());
 	for (auto &pair : a_s)
-    	if (!pair.second.empty())
-      		main.push_back(pair);
+		if (!pair.second.empty())
+			main.push_back(pair);
 	for (auto it = b_s.begin() + 1; it != b_s.end(); ++it)
- 		if (!it->second.empty())
-  			pend.push_back(*it);
+		if (!it->second.empty())
+			pend.push_back(*it);
 
 	if (!pend.empty())
 	{
@@ -236,22 +220,54 @@ void	PmergeMe::insert_vec(unsigned int elem_size)
 	_vec.clear();
 	for (auto it = main.begin(); it != main.end(); it++)
 	{
-			_vec.insert(_vec.end(), (*it).second.begin(), (*it).second.end());
+		_vec.insert(_vec.end(), (*it).second.begin(), (*it).second.end());
 	}
 
-	// Adding the remains to _vec 
+	// Adding the remains to _vec
 	_vec.insert(_vec.end(), remains.begin(), remains.end());
 	std::cout << "Vector updated with main and remains\n";
 	if (_vec.size() != _size)
 	{
 		std::cerr << "Error: _vec size mismatch after insertion. Expected: " << _size << ", Actual: " << _vec.size() << std::endl;
-		return ;
+		return;
 	}
 }
 
-// Public Methods //
+void	PmergeMe::insert_deque(unsigned int elem_size)
+{}
 
+#pragma endregion private_methods
+// -------------------------------------------------------------------------- //
+
+// ------------------------------ PUBLIC METHODS ---------------------------- //
+#pragma region public_methods
+
+// ______________________________ Utils _____________________________________ //
+#pragma region utils
 void PmergeMe::incr_nb_comps() { _nb_comps++; }
+
+// Returns the maximum number of comparisons for a given size
+// This is based on the formula derived from the Ford-Johnson algorithm
+unsigned int PmergeMe::get_max_nb_comps(unsigned int size) const
+{
+	int sum = 0;
+	for (int k = 1; k <= size; ++k)
+	{
+		double value = (3.0 / 4.0) * k;
+		sum += static_cast<int>(ceil(log2(value)));
+	}
+	return sum;
+}
+
+// Returns the nth Jacobsthal number
+long PmergeMe::get_nth_jacobsthal(unsigned int n) const
+{
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return ((pow(2, n) - pow(-1, n)) / 3);
+}
 
 // Displays the private attribute _vec
 void PmergeMe::display_vec() const
@@ -272,6 +288,8 @@ void PmergeMe::display_deq() const
 		std::cout << *it << std::endl;
 	}
 }
+#pragma endregion utils
+// __________________________________________________________________________ //
 
 // Sort _vec following Ford-Johnson algo
 void PmergeMe::sort_vector(unsigned int pair_size)
@@ -292,8 +310,10 @@ void PmergeMe::sort_deque(unsigned int pair_size)
 		sort_deque(pair_size * 2);
 		insert_deque(pair_size / 2);
 	}
-
 }
+
+#pragma endregion public_methods
+// -------------------------------------------------------------------------- //
 
 // ############################## OPERATORS ######################################################//
 
@@ -315,5 +335,5 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &copy)
 
 const std::vector<int> &PmergeMe::get_vec() const { return (_vec); }
 const std::deque<int> &PmergeMe::get_deq() const { return (_deq); }
-const unsigned int			PmergeMe::get_nb_comps() const { return (_nb_comps); }
-const unsigned int			PmergeMe::get_max_nb_comps() const { return (_max_nb_comps); }	
+const unsigned int PmergeMe::get_nb_comps() const { return (_nb_comps); }
+const unsigned int PmergeMe::get_max_nb_comps() const { return (_max_nb_comps); }
--- Projets/CPP/CPP09/ex02/PmergeMe.hpp
+++ Projets/CPP/CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: tlebon <tlebon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 16:13:05 by tlebon            #+#    #+#             */
-/*   Updated: 2025/06/24 21:51:03 by tlebon           ###   ########.fr       */
+/*   Updated: 2025/06/25 18:20:46 by tlebon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,12 @@ class PmergeMe
 		bool				safe_advance(typename T::iterator pos, unsigned int n) const;	
 		template <typename T>
 		void				sort_pairs(unsigned int pair_size);
-		unsigned int		get_max_nb_comps(unsigned int size) const;
 		Vec_pair::iterator	binary_search(Vec_pair &main, const std::pair<int, std::vector<int>> &elem,
 										Vec_pair::iterator lower_bound, Vec_pair::iterator upper_bound);
-		long				get_nth_jacobsthal(unsigned int n) const;
 		void				binary_insert(std::pair<int, std::vector<int>> &element, Vec_pair &main);
 		void				insert_pend(Vec_pair &main, Vec_pair &a_s, Vec_pair &pend);
 		void				insert_vec(unsigned int elem_size);
+		void				insert_deque(unsigned int elem_size);
 
 	public:
 		// Personnalized constructor
@@ -61,11 +60,13 @@ class PmergeMe
 		~PmergeMe();
 
 		// Methods //
-		void	incr_nb_comps();
-		void	display_vec() const;
-		void	display_deq() const;
-		void	sort_vector(unsigned int pair_size);
-		void	sort_deque(unsigned int pair_size);
+		void			incr_nb_comps();
+		unsigned int	get_max_nb_comps(unsigned int size) const;
+		long			get_nth_jacobsthal(unsigned int n) const;
+		void			display_vec() const;
+		void			display_deq() const;
+		void			sort_vector(unsigned int pair_size);
+		void			sort_deque(unsigned int pair_size);
 
 		// Operators //
 		PmergeMe&		operator = (const PmergeMe& copy);
