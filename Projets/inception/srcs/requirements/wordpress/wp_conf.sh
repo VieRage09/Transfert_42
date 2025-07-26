67d53ca03ca7428a09ace971887b7c5539282b0f
tlebon <tlebon@tlebon.42.fr.tlebon.42.fr>
2025-07-26 22:23:17 +0200

    Inception: fix de qq pb sur wordpress

--- Projets/inception/srcs/requirements/wordpress/wp_conf.sh
+++ Projets/inception/srcs/requirements/wordpress/wp_conf.sh
@@ -10,20 +10,22 @@ mv wp-cli.phar /bin/wp
 
 
 #---------------------------------------------------------------------- ??? -----#
+adduser -u 82 -D -S -G www-data www-data
 
+mkdir -p /var/www/wordpress
 cd /var/www/wordpress
-chmod -R 755 var/www/wordpress
-# chown -R www-data:www-data /var/www/wordpress
-# --> Il faut creer le groupe pour que ca fonctionne -> A voir
-
+chmod -R 755 /var/www/wordpress
+chown -R www-data:www-data /var/www/wordpress
 #----------------------------------------------------------------------------------------#
 
 
 #------------------------------------------------------------- WP core installation -----#
 # wp core download 
-wp --info
-wp core download
-wp core config --dbhost=mariadb:3306 --dbname="$DB_NAME" --dbuser"$DB_USER" --dbpass="$DB_PASSWD"
-wp core install --url="$DOMAIN_NAME" 
+wp core download --allow-root
+wp core config  --dbhost=mariadb:3306 --dbname="$DB_NAME" \
+                --dbuser="$DB_USER" --dbpass="$DB_PASSWD" --allow-root
+wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" \
+                --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWD" \
+                --admin_email="$WP_ADMIN_MAIL" --allow-root
 
 #----------------------------------------------------------------------------------------#
