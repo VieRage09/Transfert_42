6bbdabc89b17ecda008a3540a7c9f549e23e594f
tlebon <tlebon@tlebon.42.fr.tlebon.42.fr>
2025-07-26 20:02:14 +0200

    Inception: Belle avance on touche au but j'espere

--- Projets/inception/srcs/requirements/mariadb/db_conf.sh
+++ Projets/inception/srcs/requirements/mariadb/db_conf.sh
@@ -7,16 +7,21 @@ service mariadb start
 
 #---------------------------------------------------------------------- Creation DB -----#
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME};\`"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+echo "Db created"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@localhost IDENTIFIED BY '${DB_PASSWD}'"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@localhost IDENTIFIED BY '${DB_PASSWD}';"
+echo "User created"
 
 mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@localhost;"
+echo "privileges granted"
 
 mariadb -e "FLUSH PRIVILEGES;"
+echo "privileges flushed"
 
 #----------------------------------------------------------------------- Restart DB -----#
 
-mysqladmin -u root -p$DB_ROOT_PASSWD shutdown
+mysqladmin -u root -p"$DB_ROOT_PASSWD" shutdown
+echo "shutdown, launching again yeyo"
 
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
