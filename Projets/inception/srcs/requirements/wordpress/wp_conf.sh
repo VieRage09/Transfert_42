20cd0c17a073ac5348dc1c8a134189f837abf6a8
tlebon <tlebon@tlebon.42.fr.tlebon.42.fr>
2025-07-26 22:37:07 +0200

    Inception: WP a voir (ex wp-config.php not found ...)

--- Projets/inception/srcs/requirements/wordpress/wp_conf.sh
+++ Projets/inception/srcs/requirements/wordpress/wp_conf.sh
@@ -20,12 +20,14 @@ chown -R www-data:www-data /var/www/wordpress
 
 
 #------------------------------------------------------------- WP core installation -----#
-# wp core download 
 wp core download --allow-root
-wp core config  --dbhost=mariadb:3306 --dbname="$DB_NAME" \
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" 
+wp core config --dbhost=mariadb:3306 --dbname="$DB_NAME" \
                 --dbuser="$DB_USER" --dbpass="$DB_PASSWD" --allow-root
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" \
                 --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWD" \
                 --admin_email="$WP_ADMIN_MAIL" --allow-root
+
+# wp --help
 
 #----------------------------------------------------------------------------------------#
